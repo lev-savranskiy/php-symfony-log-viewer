d85d83e39ad6664aa11980d9508890d98b9e0056
Lev <lev-savranskiy@users.noreply.github.com>
2016-11-09 16:34:18 -0500

    Update logs.php

--- logs.php
+++ logs.php
@@ -9,6 +9,7 @@
  */
 
 
+
 /***
  * SETUP
  */
@@ -16,34 +17,67 @@
 //this is apache logs path
 //$p1 = "C:/xampp/apache/logs/error.log"
 //or try dynamic
+$p0 = str_replace("bin/openssl.cnf", "logs/access.log", $_SERVER["OPENSSL_CONF"]);
 $p1 = str_replace("bin/openssl.cnf", "logs/error.log", $_SERVER["OPENSSL_CONF"]);
 
 //this is Symfony  logs path
 //$p2 = "C:/Myproject/app/logs/prod.log"
 //or try dynamic
 $p2 = str_replace("web", "app/logs/prod.log", $_SERVER["DOCUMENT_ROOT"]);
+$p3 = str_replace("web", "app/logs/dev.log", $_SERVER["DOCUMENT_ROOT"]);
 
 
 
+$object =  [
+    'access' => $p0,
+    'error' => $p1,
+    'prod' => $p2,
+    'dev' => $p3
+];
 
-function logViewer($fname){
 
+/**
+ * CLEAR LOGIC
+ */
+if(isset($_GET['action']) && $_GET['action'] == 'clear'){
+    clearFile($object, $_GET['f']);
+    header("Location: logs.php");
+    // echo '<script>window.open("logs.php", "_self");</script>';
+}
+
+
+
+function clearFile($obj, $type){
+    $fname  = $obj[$type];
+    file_put_contents($fname, "");
+}
+
+
+
+function logViewer($obj, $type){
+
+    $fname  = $obj[$type];
     $file = file($fname);
 
     if(isset($_GET['f'])){
         echo "<p>Full log " . $fname . "</p>";
     }else{
-        echo "<p>See full log <a href='logs.php?f=" . $fname . "' target='_blank'>" . $fname . "</a></p>";
+        echo "<p>" . $fname . " | ";
+        echo "<a href='logs.php?f=" . $type . "' target='_blank'>See full log </a> | <a href='logs.php?f=" . $type . "&action=clear'>Clear log </a></p>";
+
     }
 
 
 
     echo '<div class="log">';
 
-    $limit = isset($_GET['f']) ? count($file) : 10;
+    $limit = isset($_GET['f']) ? 1 : count($file) - 10;
 
-    for ($i = max(0, count($file) - $limit); $i < count($file); $i++) {
-        echo "<p>" . $file[$i] . "</p>";
+    for ($i = count($file); $i > $limit; --$i) {
+        if(isset( $file[$i])){
+            echo "<p>" . $file[$i] . "</p>";
+        }
+
     }
     echo '</div>';
 }
@@ -82,12 +116,22 @@ function logViewer($fname){
 <?php
 
 if(isset($_GET['f'])){
-    logViewer($_GET['f']);
+
+
+    logViewer($object, $_GET['f']);
+
 }else{
-    echo '<h3>' . apache_get_version() . ' LOG</h3>';
-    logViewer($p1);
-    echo '<h3>SYMFONY LOG</h3>';
-    logViewer($p2);
+    echo '<h3>SYMFONY LOG PROD</h3>';
+    logViewer($object, 'prod');
+    echo '<h3>SYMFONY LOG DEV</h3>';
+    logViewer($object, 'dev');
+
+    echo '<h3>' . apache_get_version() . '</h3>';
+    echo '<h3>ACCESS</h3>';
+    logViewer($object, 'access');
+    echo '<h3>ERROR</h3>';
+    logViewer($object, 'error');
+
 }
 
 
